0bb233368263f81a50771a568c80b5650a3910b0
adm <vkolosov1@mail.ru>
2025-02-20 17:37:06 +0300

    paranoia

--- src/components/usefullToast.tsx
+++ src/components/usefullToast.tsx
@@ -36,7 +36,7 @@ export function UsefullDisAgreeToast() {
           variant: "destructive",
           title: "Uh oh! Something went wrong.",
           description: "There was a problem with your request.",
-          action: <ToastAction altText="Try again">Kill yourself</ToastAction>,
+          action: <ToastAction altText="Try again">Try again</ToastAction>,
         })
       }}
     >

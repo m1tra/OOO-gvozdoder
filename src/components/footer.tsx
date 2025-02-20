b73257c1854b988ceccbd2e1f190eb6ead8a3b29
adm <vkolosov1@mail.ru>
2025-02-20 19:52:47 +0300

    add footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,8 @@
 'use client'
+
+import Link from "next/link";
 import { usePathname } from "next/navigation"
+
 
 export default function Footer() {
   const path = usePathname();
@@ -7,8 +10,21 @@ export default function Footer() {
   
   return (
     path !== '/' ? (
-      <footer className='z-50 h-52 bg-neutral-950/90 backdrop-blur border-t flex items-center border-neutral-700'>
-        
+      <footer className='z-50  bg-neutral-950/90 backdrop-blur  flex items-center justify-center py-2 px-1 h-10'>
+        <div className='flex gap-2 lg:gap-4 '>
+          <Link href="/">
+            <div className='text-neutral-300 text-xs'>Gvozdoder</div>
+          </Link>
+          <Link href="/about-us">
+            <div className='text-neutral-300 text-xs'>About-us</div>
+          </Link>
+          <Link href="/order">
+            <div className='text-neutral-300 text-xs'>Order gvozdi</div>
+          </Link>
+          <Link href="/returns">
+            <div className='text-neutral-300 text-xs'>Exchanges & Returns</div>
+          </Link>
+        </div>
       </footer>
     ) : null
   );
